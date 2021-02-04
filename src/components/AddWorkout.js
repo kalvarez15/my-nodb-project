1cbed8a94ea3f698b3461b899bc5426a415c10ad
Kyle Alvarez <kylealvarez0001@gmail.com>
2021-02-03 19:10:56 -0600

    created components for my app

---
+++ src/components/AddWorkout.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import AddWorkoutInputs from './AddWorkoutInputs';
+
+function AddWorkout() {
+    return(<div>Add Workout <AddWorkoutInputs/> </div>)
+}
+
+export default AddWorkout
---
+++ src/components/AddWorkoutInputs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class AddWorkoutInputs extends React.Component {
+    constructor() {
+        super()
+    } 
+    render() {
+        return(<div> Add Workout Inputs</div>)
+    }
+}
+
+export default AddWorkoutInputs
